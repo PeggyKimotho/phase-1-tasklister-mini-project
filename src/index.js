d209edc054a6448b53f02b16dd9185f5954fe45a
Peggy <peggykimotho@gmail.com>
2023-06-23 11:52:45 +0000

    Completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById('create-task-form');
+  const inputBox = document.getElementById('new-task-description');
+  const tasks = document.getElementById('tasks');
+
+  function addTask(e) {
+    e.preventDefault();
+
+      let li = document.createElement('li');
+      li.textContent = inputBox.value;
+      tasks.appendChild(li);
+      let span = document.createElement('span');
+      span.textContent = '   x';
+      li.appendChild(span); 
+      inputBox.value = "";
+    
+  }
+  form.addEventListener('submit', addTask)
+
+  tasks.addEventListener('click', (e) => {
+    e.target.parentElement.remove();
+  })
 });
